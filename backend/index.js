c045e54d2d88178320a1a4b36bf23ff8a0a514e8
Youcef <mtn.youssef.kh@gmail.com>
2024-04-08 01:12:41 +0200

    final,its fixed

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,29 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const fs = require('fs');
-// const https = require('https'); // Commenté pour désactiver HTTPS
+// const fs = require('fs');
+// // const https = require('https'); // Commenté pour désactiver HTTPS
 const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const cors = require('cors');
-const morgan = require('morgan');
-const winston = require('./config/winston');
-const rateLimit = require('./utils/rateLimit');
-const errorHandler = require('./middleware/errorMiddleware');
-const verifyToken = require('./middleware/authMiddleware');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+// const mongoose = require('mongoose');
+// const helmet = require('helmet');
+// const cors = require('cors');
+// const morgan = require('morgan');
+// const winston = require('./config/winston');
+// const rateLimit = require('./utils/rateLimit');
+// const errorHandler = require('./middleware/errorMiddleware');
+// const verifyToken = require('./middleware/authMiddleware');
+// const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
 
-const utilisateursRoutes = require('./routes/utilisateurs');
-const filmsRoutes = require('./routes/films');
-const acteursRoutes = require('./routes/acteurs');
-const seriesRoutes = require('./routes/series');
-const authRoutes = require('./routes/authRoutes');
+// const utilisateursRoutes = require('./routes/utilisateurs');
+// const filmsRoutes = require('./routes/films');
+// const acteursRoutes = require('./routes/acteurs');
+// const seriesRoutes = require('./routes/series');
+// const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const RegisterModel = require('./models/Register')
+// const mongoose = require('mongoose')
+// const cors = require('cors')
+// const RegisterModel = require('./models/Register')
 
 
 // Logging des requêtes HTTP avec Morgan et Winston
@@ -41,11 +39,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
-app.use('/api/authRoutes', authRoutes);
-app.use('/api/utilisateurs', utilisateursRoutes);
-app.use('/api/films', filmsRoutes);
-app.use('/api/acteurs', acteursRoutes);
-app.use('/api/series', seriesRoutes);
+// app.use('/api/authRoutes', authRoutes);
+// app.use('/api/utilisateurs', utilisateursRoutes);
+// app.use('/api/films', filmsRoutes);
+// app.use('/api/acteurs', acteursRoutes);
+// app.use('/api/series', seriesRoutes);
 
 // // Integration de Swagger UI
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -55,8 +53,8 @@ app.use('/api/series', seriesRoutes);
 
 // Connexion à MongoDB et démarrage du serveur
 
-mongoose.connect(process.env.DBURI)
-    .catch((err) => winston.error(err.message));
+// mongoose.connect(process.env.DBURI)
+//     .catch((err) => winston.error(err.message));
 
 app.listen(4000, () => {
     console.log('HTTP Server running on port  4000');
