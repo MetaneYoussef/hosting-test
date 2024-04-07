6e9df40a68b53ddf0e915181c2c1c83778edfb99
Youcef <mtn.youssef.kh@gmail.com>
2024-04-08 01:49:12 +0200

    fixing

--- backend/api/index.js
+++ backend/api/index.js
@@ -36,6 +36,9 @@ const app = express();
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
