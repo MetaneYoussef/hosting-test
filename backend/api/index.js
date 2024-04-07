96ad6b9f4ebdaf8b0dc3feb8cbf0b8f9facd9447
Youcef <mtn.youssef.kh@gmail.com>
2024-04-08 01:32:37 +0200

    final,its fixed

--- backend/api/index.js
+++ backend/api/index.js
@@ -36,7 +36,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello World!' });
 });
 // app.use('/api/authRoutes', authRoutes);
