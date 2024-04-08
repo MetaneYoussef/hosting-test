c248f2da7529ee07ecdae34c910863fc23915cb8
Youcef <mtn.youssef.kh@gmail.com>
2024-04-08 02:45:46 +0200

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,60 +1,24 @@
-// require('dotenv').config();
-
-// const fs = require('fs');
-// // const https = require('https'); // Commenté pour désactiver HTTPS
-const express = require('express');
-// const mongoose = require('mongoose');
-// const helmet = require('helmet');
-// const cors = require('cors');
-// const morgan = require('morgan');
-// const winston = require('./config/winston');
-// const rateLimit = require('./utils/rateLimit');
-// const errorHandler = require('./middleware/errorMiddleware');
-// const verifyToken = require('./middleware/authMiddleware');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-
-// const utilisateursRoutes = require('./routes/utilisateurs');
-// const filmsRoutes = require('./routes/films');
-// const acteursRoutes = require('./routes/acteurs');
-// const seriesRoutes = require('./routes/series');
-// const authRoutes = require('./routes/authRoutes');
-
-const app = express();
-// const mongoose = require('mongoose')
-// const cors = require('cors')
-// const RegisterModel = require('./models/Register')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 
-// Logging des requêtes HTTP avec Morgan et Winston
-// app.use(morgan('combined', { stream: { write: (message) => winston.info(message.trim()) } }));
+const app = express()
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
 
-// Limite le taux de requêtes pour prévenir les attaques DDoS ou de force brute
-// app.use(rateLimit);
-
-// Middleware pour parser le corps des requêtes en JSON
-// app.use(express.json());
-
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+    res.json("Hello");
 })
-// app.use('/api/authRoutes', authRoutes);
-// app.use('/api/utilisateurs', utilisateursRoutes);
-// app.use('/api/films', filmsRoutes);
-// app.use('/api/acteurs', acteursRoutes);
-// app.use('/api/series', seriesRoutes);
 
-// // Integration de Swagger UI
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// // Gestion globale des erreurs
-// app.use(errorHandler);
 
-// Connexion à MongoDB et démarrage du serveur
-
-// mongoose.connect(process.env.DBURI)
-//     .catch((err) => winston.error(err.message));
-app.listen(process.env.PORT, () => {
-    console.log(`Serveur démarré sur le port ${process.env.PORT}`);
-});
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
