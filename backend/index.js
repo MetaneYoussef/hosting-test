3e7c76ca9b6caefe37d80ece1794d554effdf19e
Youcef <mtn.youssef.kh@gmail.com>
2024-04-08 03:01:50 +0200

    changes

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require('./models/Register')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://hosting-test-delta.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
