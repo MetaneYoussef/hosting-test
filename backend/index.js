e594a20997fa27a750cdcc8df75d06a0ae96d376
Youcef <mtn.youssef.kh@gmail.com>
2024-04-08 02:49:03 +0200

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
+const RegisterModel = require('./models/Register')
 
 const app = express()
 app.use(cors(
@@ -13,10 +13,25 @@ app.use(cors(
 ));
 app.use(express.json())
 
+mongoose.connect('mongodb+srv://youcef:maou123.@firstcluster.vooryrp.mongodb.net/MoviesDBTest?retryWrites=true&w=majority&appName=MoviesDBTest');
+
+
 app.get("/", (req, res) => {
     res.json("Hello");
 })
-
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    RegisterModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json("Already have an account")
+            } else {
+                RegisterModel.create({ name: name, email: email, password: password })
+                    .then(result => res.json(result))
+                    .catch(err => res.json(err))
+            }
+        }).catch(err => res.json(err))
+})
 
 
 app.listen(3001, () => {
